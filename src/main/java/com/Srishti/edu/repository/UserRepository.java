e8c780640a12934b42fa63386ea6cec14adc7ba6
JesnaJP <jesnajayaprakash1994@gmail.com>
2025-01-05 16:47:27 +0530

    package name change

--- src/main/java/com/Srishti/edu/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Srishti.edu.repository;
-
-import com.Srishti.edu.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
